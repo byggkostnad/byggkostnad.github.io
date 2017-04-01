8239f8c7c2123d521df4aa450e7486407ec94fcc
fTops <theof.tops@gmail.com>
2017-04-01 17:53:31 +0200

    Early attempt at matching the design with the first elements.

--- js/init.js
+++ js/init.js
@@ -1,45 +1,8 @@
-(function ($, toastr) {
-  function successFullyPostedToGoogle(){
-    //Success Message
-    toastr.info("We have received your information and will be in touch!");
-  }
-  function failedToPostToGoogle(){
-    toastr.error("We where unable to receive your information!");    
-  }
-
-  function postToGoogle() {
-    console.log("posting form to google");
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var phone = $("#phone").val();
-
-    $.ajax({
-      url: "https://docs.google.com/forms/d/104nEhIs3OasjFn9Px-oZt4lS88W-qb5CpmtNBpmepG8/formResponse",
-      data: {
-        "entry.504123295": name,
-        "entry.932829697": email,
-        "entry.920121692": phone
-      },
-      crossDomain: true,
-      type: "POST",
-      dataType: "xml",
-      success: successFullyPostedToGoogle,
-      error: failedToPostToGoogle
-    });
-    return false;
-  }
-
-
-  $(function () {
+(function($){
+  $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
   }); // end of document ready
-
-
-  $(function () {
-    $("#contact-form").submit(postToGoogle);
-  });
-
-})(jQuery, toastr); // end of jQuery name space
+})(jQuery); // end of jQuery name space
