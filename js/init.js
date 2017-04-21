611285c0facd4c2e71d918be84f92dfd8ddfe875
fTops <theof.tops@gmail.com>
2017-04-21 18:01:30 +0200

    Fixes for mobile, general improvements, and integration of toastr + google form js code.

--- js/init.js
+++ js/init.js
@@ -1,10 +1,46 @@
-(function($){
-  $(function(){
+$('.carousel.carousel-slider').carousel({fullWidth: true});
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+(function ($, toastr) {
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+	$(function () {
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+	}); // end of document ready
 
- $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+	$(function () {
+		$("#contact-form").submit(postToGoogle);
+	});
+
+	function postToGoogle() {
+		console.log("posting form to google");
+		var name = $("#name").val();
+		var email = $("#email").val();
+		var phone = $("#phone").val();
+
+		$.ajax({
+			url: "https://docs.google.com/forms/d/104nEhIs3OasjFn9Px-oZt4lS88W-qb5CpmtNBpmepG8/formResponse",
+			data: {
+				"entry.504123295": name,
+				"entry.932829697": email,
+				"entry.920121692": phone
+			},
+			crossDomain: true,
+			type: "POST",
+			dataType: "xml",
+			success: successfullyPostedToGoogle,
+			error: failedToPostToGoogle
+		});
+		return false;
+	}
+	
+	function successfullyPostedToGoogle() {
+    	//Success Message
+    	toastr.info("We have received your information and will be in touch!");
+	}
+	
+	function failedToPostToGoogle() {
+		toastr.error("We were unable to receive your information!");    
+	}
+
+})(jQuery, toastr); // end of jQuery name space
